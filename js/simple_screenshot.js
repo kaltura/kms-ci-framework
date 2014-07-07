646f072cd4116a8c28c53179dc0584410c13899c
Ori Hoch <ori.hoch@kaltura.com>
2014-07-07 12:03:55 +0300

    add support for redirects in screenshot helper

--- js/simple_screenshot.js
+++ js/simple_screenshot.js
@@ -2,19 +2,12 @@
  * take a screenshot of a url
  */
 
+var fs = require('fs');
 var system = require('system');
-if (system.args.length != 6) {
-    console.log('usage: phantomjs simple_screenshot.js (URL) (WIDTH) (HEIGHT) (IMG_FILE) (HTML_FILE)');
-    phantom.exit();
-} else {
-    var url = system.args[1];
-    var width = system.args[2];
-    var height = system.args[3];
-    var img_file = system.args[4];
-    var html_file = system.args[5];
+
+var main = function(cb, url, width, height, img_file, html_file) {
     var page = require('webpage').create();
     page.settings.resourceTimeout = 40000;
-    var fs = require('fs');
     page.viewportSize = {'width': width, 'height': height};
     console.log('rendering '+url+' to '+img_file);
     var page_status = '';
@@ -41,12 +34,53 @@ if (system.args.length != 6) {
             console.log('warning: run time is longer then 5 seconds');
         }
         if (page_is_timeout) {
-            phantom.exit(1);
+            cb(false);
         } else if (page_status != 200) {
-            console.log('got http error '+page_status);
-            phantom.exit(1);
+            if (page_status == 301 || page_status == 302) {
+                var loc = '';
+                page_headers = JSON.parse(page_headers);
+                for (var i in page_headers) {
+                    var page_header = page_headers[i];
+                    if (page_header.name.toLowerCase() == 'location') {
+                        loc = page_header.value;
+                        break;
+                    }
+                }
+                if (loc == '') {
+                    console.log('got redirect status but could not find location header');
+                    cb(false);
+                } else {
+                    if (loc.indexOf('http') != 0) {
+                        var hostname = url.split('//')[1].split('/')[0];
+                        var prot = url.split('//')[0];
+                        loc = prot+'//'+hostname+loc;
+                    }
+                    main(cb, loc, width, height, img_file, html_file);
+                }
+            } else {
+                console.log('got http error '+page_status);
+                cb(false);
+            }
         } else {
-            phantom.exit(0);
+            cb(true);
         }
     });
+};
+
+if (system.args.length != 6) {
+    console.log('usage: phantomjs simple_screenshot.js (URL) (WIDTH) (HEIGHT) (IMG_FILE) (HTML_FILE)');
+    phantom.exit();
+} else {
+    var url = system.args[1];
+    var width = system.args[2];
+    var height = system.args[3];
+    var img_file = system.args[4];
+    var html_file = system.args[5];
+    main(function(ret) {
+        if (ret) {
+            phantom.exit(0);
+        } else {
+            phantom.exit(1);
+        }
+    }, url, width, height, img_file, html_file);
 }
