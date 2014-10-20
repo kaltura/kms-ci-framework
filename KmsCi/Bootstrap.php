c68f3ac8a52fd477a480d7c079f2ae4cebf9503b
Ori Hoch <ori.hoch@kaltura.com>
2014-10-20 16:59:32 +0300

    added caching of runner and integration objects in bootstrap

--- KmsCi/Bootstrap.php
+++ KmsCi/Bootstrap.php
@@ -2,23 +2,30 @@
 
 class KmsCi_Bootstrap {
 
+    protected static $_runner = null;
+
+    protected static $_integration = null;
+
     /**
      * @return KmsCi_CliRunnerAbstract
      */
     public static function getRunner()
     {
-        if (getenv('KMSCI_RUNNER_PATH')) {
-            $configPath = getenv('KMSCI_RUNNER_PATH');
-            $configManager = new KmsCi_Config_Manager($configPath);
-            $config = $configManager->getConfig();
-            require_once($config['CliRunnerFile']);
-            $className = $config['CliRunnerClass'];
-            $args = getenv('KMSCI_RUNNER_ARGS');
-            $args = empty($args) ? array() : json_decode($args, true);
-            return new $className($config, $args, $configPath);
-        } else {
-            return false;
+        if (is_null(self::$_runner)) {
+            if (getenv('KMSCI_RUNNER_PATH')) {
+                $configPath = getenv('KMSCI_RUNNER_PATH');
+                $configManager = new KmsCi_Config_Manager($configPath);
+                $config = $configManager->getConfig();
+                require_once($config['CliRunnerFile']);
+                $className = $config['CliRunnerClass'];
+                $args = getenv('KMSCI_RUNNER_ARGS');
+                $args = empty($args) ? array() : json_decode($args, true);
+                self::$_runner = new $className($config, $args, $configPath);
+            } else {
+                self::$_runner = false;
+            }
         }
+        return self::$_runner;
     }
 
     /**
@@ -27,13 +34,16 @@ class KmsCi_Bootstrap {
      */
     public static function getIntegration($runner)
     {
-        if (getenv('KMSCI_INTEGRATION_ID')) {
-            $classname = KmsCi_Runner_IntegrationTests::getIntegrationClassById(getenv('KMSCI_INTEGRATION_ID'), $runner);
-            $integration = new $classname($runner, getenv('KMSCI_INTEGRATION_ID'));
-            return $integration;
-        } else {
-            return false;
+        if (is_null(self::$_integration)) {
+            if (getenv('KMSCI_INTEGRATION_ID')) {
+                $classname = KmsCi_Runner_IntegrationTests::getIntegrationClassById(getenv('KMSCI_INTEGRATION_ID'), $runner);
+                $integration = new $classname($runner, getenv('KMSCI_INTEGRATION_ID'));
+                self::$_integration = $integration;
+            } else {
+                self::$_integration = false;
+            }
         }
+        return self::$_integration;
     }
 
 }
