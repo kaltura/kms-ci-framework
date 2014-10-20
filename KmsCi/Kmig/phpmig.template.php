50cf693cc5f94c59cce3f0f49b6e011f6d684581
Ori Hoch <ori.hoch@kaltura.com>
2014-10-20 16:45:51 +0300

    use the KmsCi_Bootstrap for the migrations as well

--- KmsCi/Kmig/phpmig.template.php
+++ KmsCi/Kmig/phpmig.template.php
@@ -12,21 +12,6 @@ $container['phpmig.adapter'] = function($c) {
 
 $container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'migrations';
 
-$container['kmsci.runner'] = function($c) {
-    if (getenv('KMSCI_RUNNER_PATH')) {
-        $configPath = getenv('KMSCI_RUNNER_PATH');
-        $configManager = new KmsCi_Config_Manager($configPath);
-        $config = $configManager->getConfig();
-        require_once($config['CliRunnerFile']);
-        $className = $config['CliRunnerClass'];
-        $args = getenv('KMSCI_RUNNER_ARGS');
-        $args = empty($args) ? array() : json_decode($args, true);
-        return new $className($config, $args, $configPath);
-    } else {
-        return false;
-    }
-};
-
 /**POST_CODE**/
 
 return $container;
